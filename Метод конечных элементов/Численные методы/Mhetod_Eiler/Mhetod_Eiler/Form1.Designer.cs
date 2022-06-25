ad9a3b8c65fb4214207f180c3b8756c00bf0bce5
AlekseyPanaskin <EngineeringSoftwareMospolytech@yandex.ru>
2022-06-25 14:55:23 +0300

    эйлер

---
+++ Метод конечных элементов/Численные методы/Mhetod_Eiler/Mhetod_Eiler/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Mhetod_Eiler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 126);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(501, 312);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(82, 9);
+            this.label1.MaximumSize = new System.Drawing.Size(700, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(638, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Решение дифференциального уравнения первого порядка с начальными условиями методо" +
+    "м Эйлера";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(530, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Начальные условия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(530, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "x0 - левая граница";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(530, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "X - правая граница";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(530, 306);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "N - количество шагов";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(680, 247);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(680, 275);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(530, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Функция y\'=f(x, y)";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(680, 303);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(581, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 31);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(530, 222);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 16);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "y0";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox6.Location = new System.Drawing.Point(680, 219);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(9, 62);
+            this.label10.MaximumSize = new System.Drawing.Size(750, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(737, 32);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Рассмотрим дифференциальное уравнение первого порядка y0 = f(x, y), для которого " +
+    "требуется найти частное решение на отрезке [x0, X], соответствующее начальному у" +
+    "словию y(x0) = y0\r\n";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "y+x^2",
+            "5x+3y",
+            "y+e^x",
+            "sin(x)+sin(y)"});
+            this.comboBox1.Location = new System.Drawing.Point(533, 153);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(247, 24);
+            this.comboBox1.TabIndex = 18;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Метод конечных элементов/Численные методы/Mhetod_Eiler/Mhetod_Eiler/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ZedGraph;
+
+namespace Mhetod_Eiler
+{
+    public partial class Form1 : Form
+    {
+        public static int n = 0, k=0;
+        public static double h=0.1, x0=0, y0=1, X=2, s;
+        public static Array x, y;
+        public static Array x1, y1;
+       
+
+        private ZedGraphControl ZedGraph1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //comboBox1.Text = Convert.ToString("f(x, y)");
+           // this.comboBox1.Items.AddRange(new object[] { "y+x^2", "5x+3y", "y+e^x", "sin(x)+sin(y)" });
+
+
+            ZedGraph1 = zedGraphControl1;
+            GraphPane pane = ZedGraph1.GraphPane;
+            ZedGraph1.RestoreScale(pane);
+            pane.XAxis.MajorGrid.IsVisible = true;
+            pane.Title.Text = "Метод Эйлера";
+            pane.XAxis.Title.Text = "x";
+            pane.YAxis.Title.Text = "y";
+            pane.XAxis.MajorGrid.DashOn = 10;
+            pane.XAxis.MajorGrid.DashOff = 5;
+            pane.YAxis.MajorGrid.IsVisible = true;
+            pane.YAxis.MajorGrid.DashOn = 10;
+            pane.YAxis.MajorGrid.DashOff = 5;
+            pane.YAxis.MinorGrid.IsVisible = true;
+            pane.YAxis.MinorGrid.DashOn = 1;
+            pane.YAxis.MinorGrid.DashOff = 2;
+            pane.XAxis.MinorGrid.IsVisible = true;
+            pane.XAxis.MinorGrid.DashOn = 1;
+            pane.XAxis.MinorGrid.DashOff = 2;
+
+        }
+
+        
+
+        public double func(double x, double y, int k)
+        {
+            
+            if (k==0)
+            {
+                s= y+Math.Pow(x,2);
+            }
+            if (k == 1)
+            {
+                s = 5 * x + 3 * y;
+            }
+            if (k == 2)
+            {
+                s = y + Math.Exp(x);
+            }
+            if (k == 3)
+            {
+                s= Math.Sin(x)+Math.Sin(y);
+            }
+
+            return (s);
+
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            k=comboBox1.SelectedIndex;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            x0=Convert.ToDouble(textBox2.Text);
+            /*bool check = true;
+            check &= double.TryParse(textBox2.Text, out a);
+            if (!check)
+            {
+                MessageBox.Show("Ошибка! Проверьте формат вводимых значений!");
+            }*/
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            X=Convert.ToDouble(textBox3.Text);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            n=Convert.ToInt32(textBox4.Text);
+        }
+
+       
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            y0=Convert.ToDouble(textBox6.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            h = (X-x0)/ n;
+            
+            double[] x = new double[n+1];
+            double[] y = new double[n+1];
+            double[] x1 = new double[n + 1];
+            double[] y1 = new double[n + 1];
+
+            //Метод Эйлера
+
+            x[0] = x0;
+            y[0] = y0;
+
+            for (int i = 0; i < n; ++i)// Цикл для заполнения массива значениями по оси Х для графика функции
+            {
+                x[i + 1] = x[i] + h;
+                if (x[i] == X)
+                    break;
+            }
+                       
+
+            for (int i = 1; i < n+1; ++i)// Цикл для заполнения массива значениями по оси Y для графика функции
+            {
+                y[i] = y[i - 1] + h * func(x[i - 1], y[i - 1], k);
+            }
+
+            zedGraphControl1.GraphPane.CurveList.Clear();
+            PointPairList list = new PointPairList();
+
+            
+
+            for (int j = 0; j < n+1; ++j)
+            {
+                list.Add(x[j], y[j]);
+               
+            }
+            
+            LineItem MyLine = zedGraphControl1.GraphPane.AddCurve("Метод Эйлера", list, Color.Red, SymbolType.None);
+             MyLine.Line.Width = 3;
+            // MyLine.Line.Fill = new Fill(Color.AliceBlue);
+             MyLine.Symbol.IsVisible = false;
+             zedGraphControl1.RestoreScale(zedGraphControl1.GraphPane);
+
+            //Метод Эйлера с перcчетом
+
+            x1[0] = x0;
+            y1[0] = y0;
+
+            for (int i = 0; i < n; ++i)// Цикл для заполнения массива значениями по оси Х для графика функции
+            {
+                x1[i + 1] = x1[i] + h;
+                if (x1[i] == X)
+                    break;
+            }
+
+
+            for (int i = 1; i < n + 1; ++i)// Цикл для заполнения массива значениями по оси Y для графика функции
+            {
+               y1[i] = y1[i - 1] + h/2*(func(x1[i-1],y1[i-1], k)+func(x1[i], y1[i-1]+h*func(x1[i-1], y1[i-1], k),k));
+
+            }
+
+            PointPairList list2 = new PointPairList();
+
+            for (int j = 0; j < n + 1; ++j)
+            {
+                list2.Add(x1[j], y1[j]);
+
+            }
+
+            LineItem MyLine2 = zedGraphControl1.GraphPane.AddCurve("Метод Эйлера с персчетом", list2, Color.Blue, SymbolType.None);
+            MyLine2.Line.Width = 3;
+            // MyLine.Line.Fill = new Fill(Color.AliceBlue);
+            MyLine2.Symbol.IsVisible = false;
+            zedGraphControl1.RestoreScale(zedGraphControl1.GraphPane);
+
+
+        }
+    }
+}
